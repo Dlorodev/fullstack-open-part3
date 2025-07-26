3f65b5776830ff7e15b8c9e32d6f4595cc3b0db8
Dlorodev <dlorodev@gmail.com>
2025-07-25 20:52:32 -0500

    add the exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
+
 const app = express();
 const PORT = 3001;
 app.use(express.json());
+app.use(cors());
 
 let persons = [
     {
